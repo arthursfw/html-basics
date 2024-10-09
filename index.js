bc5a1d96a587b5aedf296995199ac588d01aaa50
Arthur Meneses <arthur.adm015@gmail.com>
2024-10-09 15:15:55 -0300

    Add initial html, styles and js

---
+++ index.js
@@ -0,0 +1,11 @@
+// alert("teste")
+// prompt("teste2")
+const ANO_NASCIMENTO = 2006
+let nome = "Arthur"
+console.log(ANO_NASCIMENTO, nome, "manoel gomes")
+
+// let teste_variavel
+// let $teste
+// let teste123
+// let testeVariavel
+// const TESTE_VARIAVEL
